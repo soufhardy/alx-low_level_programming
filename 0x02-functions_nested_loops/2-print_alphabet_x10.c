6bd7c066164836000563da4487bb1e31600da205
Your Name <you@example.com>
2023-07-18 08:55:32 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,21 @@
 
 /**
  * main - Entry point
- * description: 'prints 10 times the alphabet, in lowercase, followed by a new line.'
- * Return: Always 0
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
+	char ch;
 	int i;
-	int j;
-
-	for (j = 1; i <= 10; i++)
+	i = 0;
+	while (i < 10)
 	{
-		for (j = 97; j <= 122; j++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-			_putchar(j);
+			_putchar(ch);
 		}
 		_putchar('\n');
+		i++;
 	}
 }
