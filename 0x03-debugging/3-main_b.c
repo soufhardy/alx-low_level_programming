df6496dc407c63a2d5d5ea57513e63665172c79a
Your Name <you@example.com>
2023-07-20 13:04:20 -0700

    debug

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -17,6 +17,9 @@ int main(void)
     day = 01;
     year = 1997;
 
+    month = 2;
+    day = 29;
+    year = 2000;
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
     day = convert_day(month, day);
